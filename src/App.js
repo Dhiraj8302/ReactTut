7d9ad20670e5734f17e5e6a9fec056193a2bed0f
dhiraj830 <dhibhosale832@gmaie.com>
2024-02-16 16:55:23 +0530

    enabled dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Textform from './Components/Textform';
 import Navbar from './Components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [mode,setMode] = useState('light');
+
+  const toggleMode =()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-      <Navbar title="YoYO" about="About us"/>
+      <Navbar title="YoYO" about="About us" mode={mode} toggleMode={toggleMode}/>
       <div className="container mb-3">
-        <Textform heading="Enter the text to Analyse"/>
+        <Textform heading="Enter Text To Analyse" mode={mode}/>
       </div>
     </>
   );
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -11,6 +11,22 @@ export default function Textform(props) {
         let newText1=text.toLowerCase();
         setText(newText1)
     }
+
+    const handleCapitalise = () => {
+        const finalSentence = text.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+        setText(finalSentence);
+    };
+
+
+    const Clear=()=>{
+        
+        setText("")
+    }
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
     const handleOnChange=(event)=>{
         //console.log("On click");
         setText(event.target.value);
@@ -18,19 +34,23 @@ export default function Textform(props) {
     const [text, setText] = useState('')
   return (
     <>
-        <div className="container">
+        <div className="container" style={{color :props.mode==='dark'?'white':'#042743'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor :props.mode==='dark'?'grey':'white',color :props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUPclick}>To Uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLOclick}>To Lowwercase</button>
+                <button className="btn btn-info mx-1" onClick={handleUPclick}>To UPPERCASE</button>
+                <button className="btn btn-info mx-1" onClick={handleLOclick}>To LOWERCASE</button>
+                <button className="btn btn-info mx-1" onClick={handleCapitalise}>Captalise</button>
+                <button className="btn btn-danger mx-1" onClick={Clear}>Clear</button>
+                <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color :props.mode==='dark'?'white':'#042743'}}>
                 <h2>Text Summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
-                <h2>prewiew</h2>
-                <p>{text}</p>
+                <p>{text.split(" ").length * 0.008} Minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter Something to get preview"}</p>
         </div>
     </>
   )
