9c2a5681c8eab6096360482c09bad064805a8a7b
dhiraj830 <dhibhosale832@gmaie.com>
2024-02-26 14:09:23 +0530

    19th

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -40,18 +40,18 @@ export default function Textform(props) {
         <div className="container" style={{color :props.mode==='dark'?'white':'#042743'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" value={text} style={{backgroundColor :props.mode==='dark'?'grey':'white',color :props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor :props.mode==='dark'?'#161b3b':'white',color :props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-info mx-1" onClick={handleUPclick}>To UPPERCASE</button>
-                <button className="btn btn-info mx-1" onClick={handleLOclick}>To LOWERCASE</button>
-                <button className="btn btn-info mx-1" onClick={handleCapitalise}>Captalise</button>
-                <button className="btn btn-danger mx-1" onClick={Clear}>Clear</button>
+                <button className="btn btn-info mx-1 my-1" onClick={handleUPclick}>To UPPERCASE</button>
+                <button className="btn btn-info mx-1 my-1" onClick={handleLOclick}>To LOWERCASE</button>
+                <button className="btn btn-info mx-1 my-1" onClick={handleCapitalise}>Captalise</button>
+                <button className="btn btn-danger mx-1 my-1" onClick={Clear}>Clear</button>
                 <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
         </div>
         <div className="container my-3" style={{color :props.mode==='dark'?'white':'#042743'}}>
                 <h2>Text Summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{text.split(" ").length * 0.008} Minutes to read</p>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length * 0.008} Minutes to read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:"Enter Something to get preview"}</p>
         </div>
